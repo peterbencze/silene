e1b8ab7898c1364ca6579e42d9906324c111260d
Peter Bencze <peter.bencze@logmein.com>
2020-12-16 20:58:46 +0100

    Add docstrings

--- silene/crawler.py
+++ silene/crawler.py
@@ -16,7 +16,7 @@ import asyncio
 import json
 import logging
 from abc import ABC, abstractmethod
-from typing import Optional, List
+from typing import Optional, List, Any
 
 import pyppeteer
 import syncer
@@ -38,6 +38,8 @@ logger = logging.getLogger(__name__)
 
 
 class Crawler(ABC):
+    """Base class for crawlers. All crawlers must inherit from this class."""
+
     def __init__(
             self,
             crawl_frontier: CrawlFrontier = None
@@ -57,9 +59,21 @@ class Crawler(ABC):
 
     @abstractmethod
     def configure(self) -> CrawlerConfiguration:
+        """
+        Provides configuration to the crawler.
+        This is an abstract method that needs to be implemented by the subclass.
+
+        :return: the configuration of the crawler
+        """
+
         pass
 
     def start(self) -> None:
+        """
+        Starts the crawler.
+        This method will block until the crawler is finished.
+        """
+
         self._running = True
         self._browser = syncer.sync(pyppeteer.launch())
         self._page = syncer.sync(self._browser.pages())[0]  # about:blank page
@@ -76,9 +90,25 @@ class Crawler(ABC):
         self._stop_initiated = False
 
     def crawl(self, request: CrawlRequest) -> bool:
+        """
+        Adds a crawl request to the queue.
+
+        :param request: a crawl request
+        :return: True if the request was added to the queue, False if it was filtered out
+        """
+
         return self._crawl_frontier.add_request(request)
 
     def click(self, selector: str, click_count=1) -> None:
+        """
+        Clicks element which matches selector.
+
+        :param selector: an element selector
+        :param click_count: the number of clicks, defaults to 1
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchElementError: if there is no element matching selector
+        """
+
         self._check_if_crawler_running()
 
         try:
@@ -87,6 +117,17 @@ class Crawler(ABC):
             raise NoSuchElementError(selector)
 
     def click_and_wait(self, selector: str, click_count=1, timeout=30000) -> None:
+        """
+        Clicks element which matches selector and waits for navigation.
+        Note: It's more recommended to use the crawler's crawl method to follow links.
+
+        :param selector: an element selector
+        :param click_count: the number of clicks, defaults to 1
+        :param timeout: the maximum time to wait for navigation (in milliseconds), defaults to 30000
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchElementError: if there is no element matching selector
+        """
+
         self._check_if_crawler_running()
 
         syncer.sync(asyncio.wait([
@@ -95,6 +136,15 @@ class Crawler(ABC):
         ]))
 
     def close_page(self, page: BrowserPage) -> None:
+        """
+        Closes the given page.
+        Note: There must be at least one open page.
+
+        :param page: the page to close
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise ValueError: if the given page is the last one open
+        """
+
         self._check_if_crawler_running()
 
         pages = syncer.sync(self._browser.pages())
@@ -104,34 +154,93 @@ class Crawler(ABC):
         syncer.sync(pages[page.index].close())
 
     def delete_cookie(self, cookie: Cookie) -> None:
+        """
+        Deletes the given cookie.
+
+        :param cookie: the cookie to delete
+        :raise CrawlerNotRunningError: if the crawler is not running
+        """
+
         self._check_if_crawler_running()
 
         syncer.sync(self._page.deleteCookie(cookie.as_dict()))
 
     def double_click(self, selector: str) -> None:
+        """
+        Double-clicks element which matches selector.
+
+        :param selector: an element selector
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchElementError: if there is no element matching selector
+        """
+
         self.click(selector, click_count=2)
 
-    def evaluate(self, selector: str, function: str):
+    def evaluate(self, selector: str, function: str) -> Any:
+        """
+        Executes function on an element which matches selector.
+
+        :param selector: an element selector
+        :param function: a JavaScript function to be executed (e.g. "element => element.value")
+        :return: the return value of the function
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchElementError: if there is no element matching selector
+        """
+
+        self._check_if_crawler_running()
+
         try:
             return syncer.sync(self._page.querySelectorEval(selector, function))
         except ElementHandleError:
             raise NoSuchElementError(selector)
 
     def find_element(self, selector: str) -> Optional[Element]:
+        """
+        Finds first element which matches selector.
+
+        :param selector: an element selector
+        :return: the first element which matches selector, None if no element matches
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchElementError: if there is no element matching selector
+        """
+
         self._check_if_crawler_running()
 
         element_handle = syncer.sync(self._page.querySelector(selector))
         return Element(element_handle) if element_handle else None
 
     def get_cookies(self) -> List[Cookie]:
+        """
+        Returns a list of cookies for the current page URL.
+
+        :return: list of cookies for the current page URL
+        :raise CrawlerNotRunningError: if the crawler is not running
+        """
+
         self._check_if_crawler_running()
 
         return [Cookie.from_dict(cookie) for cookie in syncer.sync(self._page.cookies())]
 
     def get_current_page(self) -> BrowserPage:
+        """
+        Returns the current open page in browser.
+
+        :return: the current open page
+        :raise CrawlerNotRunningError: if the crawler is not running
+        """
+
+        self._check_if_crawler_running()
+
         return BrowserPage(self._page_index, self._page.url, syncer.sync(self._page.title()))
 
     def get_pages(self) -> List[BrowserPage]:
+        """
+        Returns a list of pages in the browser.
+
+        :return: a list of pages
+        :raise CrawlerNotRunningError: if the crawler is not running
+        """
+
         self._check_if_crawler_running()
 
         return [
@@ -140,16 +249,40 @@ class Crawler(ABC):
         ]
 
     def get_title(self) -> str:
+        """
+        Returns the current page title.
+
+        :return: the current page title
+        :raise CrawlerNotRunningError: if the crawler is not running
+        """
+
         self._check_if_crawler_running()
 
         return syncer.sync(self._page.title())
 
     def get_url(self) -> str:
+        """
+        Returns the current page URL.
+
+        :return: the current page URL
+        :raise CrawlerNotRunningError: if the crawler is not running
+        """
+
         self._check_if_crawler_running()
 
         return self._page.url
 
     def select(self, selector: str, values: List[str]) -> List[str]:
+        """
+        Selects options in a drop-down list and returns selected values.
+
+        :param selector: an element selector
+        :param values: the list of values to select
+        :return: the selected values
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchElementError: if there is no element matching selector
+        """
+
         self._check_if_crawler_running()
 
         try:
@@ -157,7 +290,15 @@ class Crawler(ABC):
         except ElementHandleError:
             raise NoSuchElementError(selector)
 
-    def set_cookie(self, cookie: Cookie):
+    def set_cookie(self, cookie: Cookie) -> None:
+        """
+        Sets a cookie.
+
+        :param cookie: the cookie to set
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise ValueError: if the cookie cannot be set due to invalid page URL
+        """
+
         self._check_if_crawler_running()
 
         try:
@@ -166,6 +307,14 @@ class Crawler(ABC):
             raise ValueError(error)
 
     def switch_to_page(self, page: BrowserPage) -> None:
+        """
+        Switches to the given page in the browser.
+
+        :param page: the page to switch to
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchPageError: if the page does not exist
+        """
+
         self._check_if_crawler_running()
 
         try:
@@ -178,6 +327,15 @@ class Crawler(ABC):
         self._add_page_listeners(self._page)
 
     def type(self, selector: str, value: str) -> None:
+        """
+        Types value in input element which matches selector.
+
+        :param selector: an element selector
+        :param value: the value to type into an input
+        :raise CrawlerNotRunningError: if the crawler is not running
+        :raise NoSuchElementError: if there is no element matching selector
+        """
+
         self._check_if_crawler_running()
 
         try:
@@ -192,6 +350,19 @@ class Crawler(ABC):
             hidden: bool = False,
             timeout: int = 30000
     ) -> None:
+        """
+        Waits until element which matches selector appears on page.
+
+        :param selector: an element selector
+        :param visible: wait for element to be present in DOM and to be
+                        visible; i.e. to not have display: none or visibility: hidden
+                        CSS properties, defaults to False
+        :param hidden: wait for element to not be found in the DOM or to
+                       be hidden, i.e. have display: none or visibility: hidden CSS
+                       properties, defaults to False
+        :param timeout: maximum time to wait for (in milliseconds), defaults to 30000
+        """
+
         self._check_if_crawler_running()
 
         # WaitTask class is not compatible with syncer, this async method is a workaround
@@ -204,26 +375,47 @@ class Crawler(ABC):
             raise WaitTimeoutError(timeout, selector)
 
     def wait_for_timeout(self, milliseconds: int) -> None:
+        """
+        Waits for timeout.
+
+        :param milliseconds: the time to wait for (in milliseconds)
+        """
+
         self._check_if_crawler_running()
 
         syncer.sync(self._page.waitFor(milliseconds))
 
     def stop(self) -> None:
+        """
+        Stops the crawler.
+        The crawler will stop before processing the next request.
+        """
+
         self._stop_initiated = True
 
     def on_start(self) -> None:
+        """Callback which is called when the crawler starts."""
+
         logger.info('Crawler is starting')
 
     def on_request_redirect(self, response: CrawlResponse, redirected_request: CrawlRequest) -> None:
+        """Callback which is called when the request is redirected."""
+
         logger.info('Request redirect: %s -> %s', response.request, redirected_request)
 
     def on_response_success(self, response: CrawlResponse) -> None:
+        """Callback which is called with successful responses (status code in range of 200 - 299)."""
+
         logger.info('Response success: %s', response)
 
     def on_response_error(self, response: CrawlResponse) -> None:
+        """Callback which is called with unsuccessful responses (status code in range of 400 - 499 / 500 - 599)."""
+
         logger.info('Response error: %s', response)
 
     def on_stop(self) -> None:
+        """Callback which is called when the crawler stops."""
+
         logger.info('Crawler is stopping')
 
     def _check_if_crawler_running(self) -> None:
